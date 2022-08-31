8e8603f82f3afd33669c5a06466cab1ba3cc1b96
Le Bao Hiep <baohiep2013@gmail.com>
2022-08-31 21:46:17 +0700

    WIP profile

--- lib/presentation/screens/home/profile/profile-screen.dart
+++ lib/presentation/screens/home/profile/profile-screen.dart
@@ -1,13 +1,9 @@
-// import 'package:mobile_final/logic/auth_bloc/auth_bloc.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mobile_final/presentation/common/birdify.dart';
-import 'package:mobile_final/presentation/screens/home/profile/profile-archivement-screen.dart';
 import 'package:mobile_final/presentation/screens/home/profile/profile-history-screen.dart';
-
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -41,11 +37,10 @@ class _MyProfile extends StatelessWidget {
           children: [
             Container(
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/bg.png'),
-                  fit: BoxFit.cover,
-                )
-              ),
+                  image: DecorationImage(
+                image: AssetImage('assets/bg.png'),
+                fit: BoxFit.cover,
+              )),
             ),
             Align(
               alignment: Alignment.bottomCenter,
@@ -65,16 +60,7 @@ class _MyProfile extends StatelessWidget {
               top: 90.h,
               child: Column(
                 children: [
-                  Stack(
-                    children: [
-                      buildImage(),
-                      Positioned(
-                        top: 0,
-                        right: 0,
-                        child: buildEditIcon(Colors.blue),
-                      ),
-                    ],
-                  ),
+                  buildImage(),
                   SizedBox(
                     height: 10,
                   ),
@@ -83,13 +69,16 @@ class _MyProfile extends StatelessWidget {
                       children: [
                         Text(
                           'Nguyen Van A',
-                          style: TextStyle(fontSize: 28, fontFamily: 'Roboto', fontWeight: FontWeight.w700),
+                          style: TextStyle(
+                              fontSize: 28,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w700),
                         ),
                         // Icon(Icons.edit),
                         TextButton.icon(
-                          onPressed: (){}, 
-                          icon: Icon(Icons.edit), 
-                          label: Text(''), 
+                          onPressed: () {},
+                          icon: Icon(Icons.edit),
+                          label: Text(''),
                           style: TextButton.styleFrom(
                             primary: Colors.black,
                           ),
@@ -97,111 +86,55 @@ class _MyProfile extends StatelessWidget {
                       ],
                     ),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    child: Text(
-                      'Developer, Male, 20',
-                      style: TextStyle(fontSize: 20, fontFamily: 'Roboto', color: Colors.black45),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    child: Row(
-                      children: [
-                        Icon(Icons.location_pin),
-                        Text(
-                          'Ho Chi Minh City',
-                          style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
-                    )
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
+                  const SizedBox(height: 10),
                   Row(
                     children: [
-                      SizedBox(
-                        height: height / 16,
-                        width: width / 3.5,
-                        child: FlatButton(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50)),
-                          color: Colors.black,
-                          onPressed: () {},
-                          child: Text(
-                            'Overview',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 16.5,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      SizedBox(
-                        width: width / 3,
-                        height: height / 16.5,
-                        child: FlatButton(
-                          // onPressed: () => Get.to(() => const ProfileArchivementScreen()),
-                          // onPressed: (){},
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                content: MyArchivement(),
-                              ),
-                            );
-                          },
-                          child: Text(
-                            'Archivement',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 16.5,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox( width: 20),
-                      SizedBox(
-                        width: width / 4,
-                        height: height / 16.5,
-                        child: FlatButton(
-                          // onPressed: () => Get.to(() => const ProfileHistoryScreen()),
-                          // onPressed: (){},
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                content: MyHistory(),
-                              ),
-                            );
-                          },
-                          child: Text(
-                            'History',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 16.5,
-                              ),
-                            ),
+                      Icon(Icons.location_pin),
+                      Text(
+                        'Ho Chi Minh City',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 20,
                         ),
                       ),
                     ],
                   ),
-                  SizedBox(height: 20),
-                  _OverviewButtonMatches(),
-                  SizedBox(height: 35),
-                  _OverviewButtonSkill(),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: width / 4,
+                        height: height / 16.5,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            elevation: 0,
+                            primary: Colors.black,
+                            textStyle: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                ?.copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => const AlertDialog(
+                                content: MyHistory(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            'History',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 16.5,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const _OverviewButtonMatches(),
                 ],
               ),
             ),
@@ -212,7 +145,7 @@ class _MyProfile extends StatelessWidget {
   }
 
   Widget buildImage() {
-    final image = AssetImage('assets/user-avatar.png');
+    const image = AssetImage('assets/user-avatar.png');
 
     return ClipOval(
       child: Material(
@@ -222,38 +155,11 @@ class _MyProfile extends StatelessWidget {
           fit: BoxFit.cover,
           width: 128,
           height: 128,
-          child: InkWell(onTap: (){}),
+          child: InkWell(onTap: () {}),
         ),
       ),
     );
   }
-
-  Widget buildEditIcon(Color color) => buildCircle(
-    color: Colors.white,
-    all: 3,
-    child: buildCircle(
-      color: color,
-      all: 7,
-      child: Icon(
-        Icons.add_a_photo,
-        color: Colors.white,
-        size: 20,
-      ),
-    ),
-  );
-
-  Widget buildCircle({
-    required Widget child,
-    required double all,
-    required Color color,
-  }) =>
-      ClipOval(
-        child: Container(
-          padding: EdgeInsets.all(all),
-          color: color,
-          child: child,
-        ),
-      );
 }
 
 class _OverviewButtonMatches extends StatelessWidget {
@@ -268,129 +174,33 @@ class _OverviewButtonMatches extends StatelessWidget {
         padding: EdgeInsets.all(10.w),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Image.asset('assets/matches.png'),
+            SizedBox(width: 10.h),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 AutoSizeText(
-                  "Số trận đã chơi",
+                  "Attended Meet Ups",
                   style: Theme.of(context)
                       .textTheme
                       .bodyText1
                       ?.copyWith(fontWeight: FontWeight.w700),
                 ),
                 SizedBox(height: 10.h),
-                Row(
-                  children: [
-                    AutoSizeText(
-                      "19 trận",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText1
-                          ?.copyWith(fontWeight: FontWeight.w300),
-                    ),
-                    SizedBox(width: 35.w),
-                    AutoSizeText(
-                      "-",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText1
-                          ?.copyWith(fontWeight: FontWeight.w300),
-                    ),
-                    SizedBox(width: 35.w),
-                    AutoSizeText(
-                      "Kể từ 9/4/2022",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText1
-                          ?.copyWith(fontWeight: FontWeight.w300),
-                    ),
-                  ],
-                ),
-                
-              ],
-            )
-          ],
-        ),
-      ),
-      onTap: (){},
-    );
-  }
-}
-
-class _OverviewButtonSkill extends StatelessWidget {
-  const _OverviewButtonSkill({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Birdify.stackCard(
-      height: 87.h,
-      width: 362.w,
-      child: Padding(
-        padding: EdgeInsets.all(10.w),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Image.asset('assets/skill.png'),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
                 AutoSizeText(
-                  "Kỹ năng",
+                  "19 meet ups",
                   style: Theme.of(context)
                       .textTheme
                       .bodyText1
-                      ?.copyWith(fontWeight: FontWeight.w700),
-                ),
-                SizedBox(height: 10.h),
-                Row(
-                  children: [
-                    AutoSizeText(
-                      "Trung bình",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText1
-                          ?.copyWith(fontWeight: FontWeight.w300),
-                    ),
-                    SizedBox(width: 10.w),
-                    AutoSizeText(
-                      "-",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText1
-                          ?.copyWith(fontWeight: FontWeight.w300),
-                    ),
-                    SizedBox(width: 10.w),
-                    Row(
-                      children: [
-                        AutoSizeText(
-                          "Lối đánh",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1
-                              ?.copyWith(fontWeight: FontWeight.w300),
-                        ),
-                        SizedBox(width: 4.w),
-                        AutoSizeText(
-                          "Tấn công",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyText1
-                              ?.copyWith(fontWeight: FontWeight.w700),
-                        ),
-                      ],
-                    )
-                  ],
+                      ?.copyWith(fontWeight: FontWeight.w300),
                 ),
               ],
             )
           ],
         ),
       ),
-      onTap: (){},
+      onTap: () {},
     );
   }
 }
