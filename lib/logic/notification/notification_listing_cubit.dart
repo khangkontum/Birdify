5dbc470a02b2d6de14c1a81e40273e3b2caa6ff1
Le Bao Hiep <baohiep2013@gmail.com>
2022-08-31 21:08:31 +0700

    auto reload notifs

--- lib/logic/notification/notification_listing_cubit.dart
+++ lib/logic/notification/notification_listing_cubit.dart
@@ -2,17 +2,22 @@ import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mobile_final/data/models/notification.dart';
+import 'dart:async';
 import 'package:mobile_final/data/repositories/api_repository.dart';
 
 part 'notification_listing_state.dart';
 
 class ListingNotificationCubit extends Cubit<ListingNotificationState> {
   final ApiRepository apiRepository;
+  late Timer timer;
 
   ListingNotificationCubit(
     this.apiRepository,
   ) : super(ListingNotificationState.initial()) {
     listing();
+    timer = Timer.periodic(const Duration(seconds: 2), (timer) {
+      pollForUpdate();
+    });
   }
 
   Future<void> listing() async {
@@ -50,6 +55,42 @@ class ListingNotificationCubit extends Cubit<ListingNotificationState> {
       } catch (e) {
         print(e);
       }
+    }
+  }
+
+  Future<void> pollForUpdate() async {
+    if (state.status == ListingNotificationStatus.submitting ||
+        state.status == ListingNotificationStatus.error ||
+        state.status == ListingNotificationStatus.pollError) return;
+
+    try {
+      var response = await apiRepository.request(
+        postfix: '/notification/list?markRead=false',
+        method: 'GET',
+      );
+
+      var tempNotifications = <Notification>[];
+
+      for (var rawNotification in response.data['notifications']) {
+        tempNotifications.add(
+          Notification.fromJson(rawNotification),
+        );
+      }
+
+      emit(state.copyWith(
+        status: ListingNotificationStatus.success,
+        notifications: tempNotifications,
+        uid: apiRepository.currentUid,
+      ));
+    } on DioError catch (e) {
+      emit(
+        state.copyWith(
+          status: ListingNotificationStatus.pollError,
+          errorStatus: e.message,
+        ),
+      );
+    } catch (e) {
+      print(e);
     }
   }
 }
